9cc83ddb1bb0df52e33dc50116290f8b12e03671
Daniel Gift <daniel.gift@sbs.org>
2024-02-28 14:33:52 -0500

    Create Pong.java

---
+++ Pong.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+public class Pong extends JPanel implements ActionListener, KeyListener {
+   
+    public Pong() { 
+    }
+
+    public void paintComponent(Graphics g){
+    }
+
+    public void actionPerformed(ActionEvent e) {        
+    }
+  
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public void keyPressed(KeyEvent e) {
+    }
+  
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public static void main(String[] args){
+    }
+}
